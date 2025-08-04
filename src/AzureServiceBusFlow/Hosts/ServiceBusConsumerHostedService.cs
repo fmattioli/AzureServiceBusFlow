26e4205f0ac0f204417a0da63a9b2e505356d865
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2025-08-04 15:32:25 -0300

    FEAT: Forcing pipeline

--- src/AzureServiceBusFlow/Hosts/ServiceBusConsumerHostedService.cs
+++ src/AzureServiceBusFlow/Hosts/ServiceBusConsumerHostedService.cs
@@ -2,82 +2,81 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace AzureServiceBusFlow.Hosts
+namespace AzureServiceBusFlow.Hosts;
+
+public class ServiceBusConsumerHostedService(
+    Func<ServiceBusReceivedMessage, IServiceProvider, Task> messageHandler,
+    IServiceProvider serviceProvider,
+    ILogger logger,
+    string connectionString,
+    string entityName,
+    string subscriptionName = null!) : IHostedService, IAsyncDisposable
 {
-    public class ServiceBusConsumerHostedService(
-        Func<ServiceBusReceivedMessage, IServiceProvider, Task> messageHandler,
-        IServiceProvider serviceProvider,
-        ILogger logger,
-        string connectionString,
-        string entityName,
-        string subscriptionName = null!) : IHostedService, IAsyncDisposable
+    private readonly ServiceBusClient _client = new(connectionString);
+    private ServiceBusProcessor _processor = null!;
+
+    public Task StartAsync(CancellationToken cancellationToken)
     {
-        private readonly ServiceBusClient _client = new(connectionString);
-        private ServiceBusProcessor _processor = null!;
-
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
-            _processor = subscriptionName is null
-                ? _client.CreateProcessor(entityName, new ServiceBusProcessorOptions
-                {
-                    MaxConcurrentCalls = 5,
-                    MaxAutoLockRenewalDuration = TimeSpan.FromMinutes(60),
-                    AutoCompleteMessages = false,
-                })
-                : _client.CreateProcessor(entityName, subscriptionName, new ServiceBusProcessorOptions
-                {
-                    MaxConcurrentCalls = 5,
-                    MaxAutoLockRenewalDuration = TimeSpan.FromMinutes(60),
-                    AutoCompleteMessages = false,
-                });
-
-            _processor.ProcessMessageAsync += ProcessMessageHandler;
-            _processor.ProcessErrorAsync += ProcessErrorHandler;
-
-            return _processor.StartProcessingAsync(cancellationToken);
-        }
-
-        private async Task ProcessMessageHandler(ProcessMessageEventArgs args)
-        {
-            try
+        _processor = subscriptionName is null
+            ? _client.CreateProcessor(entityName, new ServiceBusProcessorOptions
             {
-                await messageHandler(args.Message, serviceProvider);
-                await args.CompleteMessageAsync(args.Message);
-            }
-            catch (Exception ex)
+                MaxConcurrentCalls = 5,
+                MaxAutoLockRenewalDuration = TimeSpan.FromMinutes(60),
+                AutoCompleteMessages = false,
+            })
+            : _client.CreateProcessor(entityName, subscriptionName, new ServiceBusProcessorOptions
             {
-                logger.LogError(ex, "Process message error: {ErrorSource}", ex.StackTrace);
-                await args.AbandonMessageAsync(args.Message);
-            }
-        }
+                MaxConcurrentCalls = 5,
+                MaxAutoLockRenewalDuration = TimeSpan.FromMinutes(60),
+                AutoCompleteMessages = false,
+            });
 
-        private Task ProcessErrorHandler(ProcessErrorEventArgs args)
+        _processor.ProcessMessageAsync += ProcessMessageHandler;
+        _processor.ProcessErrorAsync += ProcessErrorHandler;
+
+        return _processor.StartProcessingAsync(cancellationToken);
+    }
+
+    private async Task ProcessMessageHandler(ProcessMessageEventArgs args)
+    {
+        try
         {
-            logger.LogError(args.Exception, "Processor error: {ErrorSource}", args.ErrorSource);
-            return Task.CompletedTask;
+            await messageHandler(args.Message, serviceProvider);
+            await args.CompleteMessageAsync(args.Message);
         }
-
-        public async Task StopAsync(CancellationToken cancellationToken)
+        catch (Exception ex)
         {
-            if (_processor != null)
-            {
-                await _processor.StopProcessingAsync(cancellationToken);
-                await _processor.DisposeAsync();
-            }
-
-            await _client.DisposeAsync();
+            logger.LogError(ex, "Process message error: {ErrorSource}", ex.StackTrace);
+            await args.AbandonMessageAsync(args.Message);
         }
+    }
 
-        public async ValueTask DisposeAsync()
+    private Task ProcessErrorHandler(ProcessErrorEventArgs args)
+    {
+        logger.LogError(args.Exception, "Processor error: {ErrorSource}", args.ErrorSource);
+        return Task.CompletedTask;
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        if (_processor != null)
         {
-            if (_processor != null)
-            {
-                await _processor.DisposeAsync();
-            }
-
-            await _client.DisposeAsync();
-
-            GC.SuppressFinalize(this);
+            await _processor.StopProcessingAsync(cancellationToken);
+            await _processor.DisposeAsync();
         }
+
+        await _client.DisposeAsync();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_processor != null)
+        {
+            await _processor.DisposeAsync();
+        }
+
+        await _client.DisposeAsync();
+
+        GC.SuppressFinalize(this);
     }
 }
