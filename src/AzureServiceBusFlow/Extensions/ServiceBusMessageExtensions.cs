21a52cd6b4afbb676d9be3defb58cdc7821daa4c
Felipe Mattioli <felipe.mattioli@smartinnovate.com.br>
2025-10-16 22:32:41 +0200

    FEAT: Adding new changes

---
+++ src/AzureServiceBusFlow/Extensions/ServiceBusMessageExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Azure.Messaging.ServiceBus;
+
+namespace AzureServiceBusFlow.Extensions
+{
+    public static class ServiceBusMessageExtensions
+    {
+        public static string? GetStringProperty(this ServiceBusReceivedMessage message, string key)
+        {
+            return message.ApplicationProperties.TryGetValue(key, out var value)
+                ? value as string ?? value?.ToString()
+                : null;
+        }
+    }
+}
